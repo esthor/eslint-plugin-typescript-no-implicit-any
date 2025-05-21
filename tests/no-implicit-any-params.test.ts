59a522f839b12aff7749a76b00f16c34888d841e
Erik Thorelli <ethorelli@gmail.com>
2025-05-21 06:49:50 -0700

    Add RuleTester test for no-implicit-any-params

--- tests/no-implicit-any-params.test.ts
+++ tests/no-implicit-any-params.test.ts
@@ -1,3 +1,18 @@
-export const RULE_NAME = "no-implicit-any";
-export type MessageIds = "noImplicitAnyRequired";
-export type Options = [];
+import { TSESLint } from '@typescript-eslint/utils';
+import rule from '../rules/no-implicit-any-params';
+
+const ruleTester = new TSESLint.RuleTester({
+  parser: require.resolve('@typescript-eslint/parser'),
+});
+
+ruleTester.run('no-implicit-any-params', rule, {
+  valid: [
+    'function typed(a: string) {}',
+  ],
+  invalid: [
+    {
+      code: 'function untyped(a) {}',
+      errors: [{ messageId: 'noImplicitAnyRequired' }],
+    },
+  ],
+});
