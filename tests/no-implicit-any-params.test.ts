50d17d6dca8dc05422a3f7ad6a4ea5aa6d2c36bc
Erik Thorelli <ethorelli@gmail.com>
2025-05-21 07:54:43 -0700

    test: expand rule cases

--- tests/no-implicit-any-params.test.ts
+++ tests/no-implicit-any-params.test.ts
@@ -7,11 +7,36 @@ const ruleTester = new TSESLint.RuleTester({
 
 ruleTester.run('no-implicit-any-params', rule, {
   valid: [
+    // annotated parameter
     'function typed(a: string) {}',
+    // multiple annotated parameters
+    'function typedMany(a: number, b: string) {}',
+    // parameter explicitly annotated as any
+    'function explicitAny(a: any) {}',
+    // parameter with both annotation and default value
+    'const typedDefault = function(a: number = 1) {};',
+    // destructured parameter with type annotation
+    'function destructured({a}: { a: string }) {}',
   ],
   invalid: [
     {
       code: 'function untyped(a) {}',
+      output: 'function untyped(a: any) {}',
+      errors: [{ messageId: 'noImplicitAnyRequired' }],
+    },
+    {
+      code: 'const expr = function(b) {}',
+      output: 'const expr = function(b: any) {}',
+      errors: [{ messageId: 'noImplicitAnyRequired' }],
+    },
+    {
+      code: 'function multipleUntyped(a, b: string) {}',
+      output: 'function multipleUntyped(a: any, b: string) {}',
+      errors: [{ messageId: 'noImplicitAnyRequired' }],
+    },
+    {
+      code: 'function destructured({a}) {}',
+      output: 'function destructured({a}: any) {}',
       errors: [{ messageId: 'noImplicitAnyRequired' }],
     },
   ],
